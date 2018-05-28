88f138eeb252b3f2f32893b82e42a4f14a67e469
MaryZerep <38222760+MaryZerep@users.noreply.github.com>
2018-05-28 18:39:31 -0500

    2Subida

--- js/acciones.js
+++ js/acciones.js
@@ -23,14 +23,14 @@ $('#watch').on('click',function(){
 		maximumAge:3600000
 	}
 	
-	var watchID=navigator.geolocation.getCurrentPosition(onSuccess,orError.options);
+	var watchID=navigator.geolocation.getCurrentPosition(onSuccess,onError.options);
 	function onSuccess(position){
 		
 		alert('Latitude:'	+position.coords.latitude	+'\n'+
 		'Longitude:'	+position.coords.longitude	+'\n'+
 		'Altitude:'	+position.coords.altitude	+'\n'+
 		'Accuracy:'	+position.coords.accuracy	+'\n'+
-		'Altitud Accuracy:'	+position.coords.altitudAccuracy	+'\n'+
+		'Altitude Accuracy:'	+position.coords.altitudeAccuracy	+'\n'+
 		'Heading:'	+position.coords.heading	+'\n'+
 		'Speed:'	+position.coords.speed	+'\n'+
 		'Timestamp:'	+position.timestamp		+'\n');
